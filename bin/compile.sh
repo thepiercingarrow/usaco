105804c4d191377c50f008e6e343dd6be9c99777
Mark Wright <thepiercingarrow@gmail.com>
2016-07-11 10:43:34 -0400

    add -s option for compiling

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 for EXT in 'c' 'cpp'; do
     for SOURCE in $(find . -type f -name "*.${EXT}"); do
-	echo gcc $SOURCE -O2 -o $(echo $SOURCE | sed "s/\.${EXT}/\.out/g")
-	gcc $SOURCE -O2 -o $(echo $SOURCE | sed "s/\.${EXT}/\.out/g")
+	if [ ! $1 ] || test $1 != '-s' -a $1 != '--silent'; then
+            echo gcc $SOURCE -O2 -o $(echo $SOURCE | sed "s/\.${EXT}$/\.out/g")
+	fi
+	gcc $SOURCE -O2 -o $(echo $SOURCE | sed "s/\.${EXT}$/\.out/g")
     done
 done
