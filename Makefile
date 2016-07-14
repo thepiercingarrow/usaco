769514a79a9ee64e8d4bfd6c132c081230dab294
Mark Wright <thepiercingarrow@gmail.com>
2016-07-14 03:17:16 -0400

    cleanup testing

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
 all: bin/compile.sh
 	bin/compile.sh
 
-test: bin/test.sh
-	bin/test.sh
-
 clean: bin/clean.sh
 	bin/clean.sh
--- bin/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for PROG in $(find . -type f -name "*.out" | sed 's/^\.\/src\///g' | sed 's/\.out$//g'); do
-    echo $PROG
-done
