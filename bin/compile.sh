4a965953d3c9d6e8dcf4d1cd6dcc377d78075f7f
Mark Wright <thepiercingarrow@gmail.com>
2016-07-15 08:56:36 -0400

    add gcc -lm flag to appease travis

--- bin/compile.sh
+++ bin/compile.sh
@@ -6,9 +6,9 @@ for EXT in 'c' 'cpp'; do
     for SOURCE in $(find . -type f -name "*.${EXT}"); do
 	mkdir -p $(echo $SOURCE | sed 's/src/build/g' | sed 's/[a-z1-9\.]*$//g')
 	if [ ! $1 ] || test $1 != '-s' -a $1 != '--silent'; then
-            echo gcc $SOURCE -O2 -o $(echo $SOURCE | sed 's/src/build/g' | sed "s/\.${EXT}$//g")
+            echo gcc $SOURCE -O2 -o $(echo $SOURCE | sed 's/src/build/g' | sed "s/\.${EXT}$//g") -lm
 	fi
-	gcc $SOURCE -O2 -o $(echo $SOURCE | sed 's/src/build/g' | sed "s/\.${EXT}$//g")
+	gcc $SOURCE -O2 -o $(echo $SOURCE | sed 's/src/build/g' | sed "s/\.${EXT}$//g") -lm
 	if [ $? != 0 ]; then
 	    exit 1;
 	fi
